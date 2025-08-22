fe6508a709e728d8f03f8a5cad90b72a1181e7ff
Tanisha <tthompsonjones8771@gmail.com>
2025-08-21 22:49:12 -0400

    Add Connections List to components

---
+++ frontend/src/components/ConnectionsList/ConnectionsList.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import * as authService from "../services/authService";
+import "./ConnectionsList.css";
+
+export default function ConnectionsList() {
+    const [connections, setConnections] = useState([]);
+
+    useEffect(() => {
+        async function fetchConnections() {
+        const data = await authService.getConnections();
+        setConnections(data);
+    }
+    fetchConnections();
+    }, []);
+
+return (
+    <div className="connections-list">
+        <h3>My Connections</h3>
+        {connections.length === 0 ? (
+        <p>No connections yet.</p>
+        ) : (
+        <ul>
+            {connections.map((c) => (
+            <li key={c._id}>
+                <img src={c.profileImage || "/default-profile.png"} alt="" />
+                <span>
+                    {c.firstName} {c.lastName}
+                </span>
+            </li>
+        ))}
+        </ul>
+    )}
+    </div>
+);
+}
