d99637753552c31a9512892976f16a165086eddf
Tanisha <tthompsonjones8771@gmail.com>
2025-08-24 16:21:05 -0400

    Update code in api/authApi.js

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const API = axios.create({ baseURL: "http://localhost:5000/api/auth" });
 
-export const register = (payload) => API.post("/register", payload).then(r => r.data);
+export const signup = (payload) => API.post("/signup", payload).then(r => r.data);
 export const login = (payload) => API.post("/login", payload).then(r => r.data);
