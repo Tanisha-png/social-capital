eb75caa2f1f01bc387542c746597491a091d6ce4
Tanisha <tthompsonjones8771@gmail.com>
2025-08-24 16:04:35 -0400

    Update code in routes/messageRoutes.js

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -11,8 +11,14 @@
 
 // backend/routes/messageRoutes.js
 import express from "express";
+import { saveMessage, getConversation } from "../controllers/messageController.js";
+import checkToken from "../middleware/checkToken.js";
+import ensureLoggedIn from "../middleware/ensureLoggedIn.js";
 
 const router = express.Router();
+
+router.post("/", checkToken, ensureLoggedIn, saveMessage);
+router.get("/", checkToken, ensureLoggedIn, getConversation);
 
 // Example: Send a message
 router.post("/", async (req, res) => {
