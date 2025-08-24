3528252776b541c7b29c371496462cf0f092a4cb
Tanisha <tthompsonjones8771@gmail.com>
2025-08-24 16:23:50 -0400

    Update code in api/userApi.js

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 const base = axios.create({ baseURL: "http://localhost:5000/api/users" });
 
-export const getProfile = (id, token) => base.get(`/${id}`, { headers: { Authorization: `Bearer ${token}` } }).then(r => r.data);
-export const updateProfile = (data, token) => base.put("/", data, { headers: { Authorization: `Bearer ${token}` } }).then(r => r.data);
+export const getProfile = (id, token) =>
+    base.get(`/${id}`, { headers: { Authorization: `Bearer ${token}` } }).then(r => r.data);
 
-export const getFriendList = async (token) => {
-    const { data } = await axios.get('/api/users/friends', {
-        headers: { Authorization: `Bearer ${token}` }
-    });
-    return data;
-};
+export const updateProfile = (data, token) =>
+    base.put("/", data, { headers: { Authorization: `Bearer ${token}` } }).then(r => r.data);
+
+// export const getFriendList = async (token) => {
+//     const { data } = await axios.get('/api/users/friends', {
+//         headers: { Authorization: `Bearer ${token}` }
+//     });
+//     return data;
+// };
+
+export const getFriendList = (token) =>
+    base.get("/friends", { headers: { Authorization: `Bearer ${token}` } }).then(r => r.data);
