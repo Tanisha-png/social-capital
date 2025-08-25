6f69d602817ca8de3ec6d4b770d71a49f2086e7e
Tanisha <tthompsonjones8771@gmail.com>
2025-08-24 22:04:50 -0400

    Add code to prevent OverwriteModelError

--- backend/models/user.js
+++ backend/models/user.js
@@ -67,5 +67,6 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return bcrypt.compare(enteredPassword, this.password);
 };
 
-const User = mongoose.model("User", userSchema);
+// const User = mongoose.model("User", userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 export default User;
