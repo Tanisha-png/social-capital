be3a0edf25207707737587e7dcb9ef50d6a588c9
Tanisha <tthompsonjones8771@gmail.com>
2025-08-24 22:11:48 -0400

    Update code in Post.js

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,18 +1,29 @@
+import mongoose from "mongoose";
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema(
-  {
-    content: { type: String, required: true },
-    user: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: 'User'
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+// const postSchema = new Schema(
+//   {
+//     content: { type: String, required: true },
+//     user: {
+//       type: Schema.Types.ObjectId,
+//       required: true,
+//       ref: 'User'
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
 
-module.exports = mongoose.model('Post', postSchema);
+// module.exports = mongoose.model('Post', postSchema);
+
+const postSchema = new mongoose.Schema({
+  content: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Post = mongoose.models.Post || mongoose.model("Post", postSchema);
+
+export default Post;
