6e12f725778b72f50ab1159c9895932bcf89b4a1
Tanisha <tthompsonjones8771@gmail.com>
2025-08-24 22:12:55 -0400

    Update code in Message.js

--- backend/models/message.js
+++ backend/models/message.js
@@ -1,10 +1,21 @@
 import mongoose from "mongoose";
 
-const MessageSchema = new mongoose.Schema({
+// const MessageSchema = new mongoose.Schema({
+//     sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+//     receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+//     content: { type: String, required: true },
+//     read: { type: Boolean, default: false }
+// }, { timestamps: true });
+
+// export default mongoose.models.Message || mongoose.model("Message", MessageSchema);
+
+const messageSchema = new mongoose.Schema({
     sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     content: { type: String, required: true },
-    read: { type: Boolean, default: false }
-}, { timestamps: true });
+    createdAt: { type: Date, default: Date.now },
+});
 
-export default mongoose.models.Message || mongoose.model("Message", MessageSchema);
+const Message = mongoose.models.Message || mongoose.model("Message", messageSchema);
+
+export default Message;
