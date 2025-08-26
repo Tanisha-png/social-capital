32033606004e74c601f7cecf5e549cebc3061ea6
Tanisha <tthompsonjones8771@gmail.com>
2025-08-26 19:04:31 -0400

    Update code in posts.js

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -68,6 +68,7 @@ import Post from "../models/Post.js";
 export const getPosts = async (req, res) => {
   try {
     const posts = await Post.find().populate("author", "name email");
+    res.json([{ id: 1, content: "Hello World!", author: "Test User" }]);
     res.json(posts);
   } catch (err) {
     res.status(500).json({ message: "Error fetching posts" });
@@ -76,6 +77,8 @@ export const getPosts = async (req, res) => {
 
 export const createPost = async (req, res) => {
   try {
+    const { content } = req.body;
+    res.json({ id: Date.now(), content, author: "LoggedInUser" });
     const post = new Post({
       author: req.user._id,
       content: req.body.content,
