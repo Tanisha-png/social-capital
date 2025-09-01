8fb563e1cb07b3e811f3fd42a3810cb7dfcd23ed
Tanisha <tthompsonjones8771@gmail.com>
2025-08-31 22:35:35 -0400

    Update code in NewPostPage.jsx

--- frontend/src/pages/NewPostPage/NewPostPage.jsx
+++ frontend/src/pages/NewPostPage/NewPostPage.jsx
@@ -1,39 +1,3 @@
-// import { useState } from 'react';
-// import { useNavigate } from 'react-router';
-// import { useAuth } from "../../context/AuthContext";
-// import * as postService from '../../services/postService';
-
-// export default function NewPostPage() {
-//   const [content, setContent] = useState('');
-
-//   const navigate = useNavigate();
-
-//   async function handleSubmit(evt) {
-//     evt.preventDefault();
-//     try {
-//       const post = await postService.create(content);
-//       navigate('/posts');
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   }
-
-//   return (
-//     <>
-//       <h2>New Post</h2>
-//       <form autoComplete="off" onSubmit={handleSubmit}>
-//         <label>Post Content</label>
-//         <input
-//           type="text"
-//           value={content}
-//           onChange={(evt) => setContent(evt.target.value)}
-//           required
-//         />
-//         <button type="submit">ADD POST</button>
-//       </form>
-//     </>
-//   );
-// }
 
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../../context/AuthContext";
