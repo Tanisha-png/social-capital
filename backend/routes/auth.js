d5c760a9d6568e90cf9e08a10fd208e08970d3a4
Tanisha <tthompsonjones8771@gmail.com>
2025-08-31 22:18:51 -0400

    Update code in auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const authCtrl = require('../controllers/auth');
-
-// // All paths start with '/api/auth'
-
-// // POST /api/auth/signup
-// router.post('/signup', authCtrl.signUp);
-// // POST /api/auth/login
-// router.post('/login', authCtrl.logIn);
-
-// module.exports = router;
 
 // backend/routes/auth.js
 import express from "express";
