86fa5b8e3538874c43dc1b39fb79926086e86c4d
Tanisha <tthompsonjones8771@gmail.com>
2025-08-31 22:17:28 -0400

    Update code in user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,33 +1,3 @@
-// import mongoose from "mongoose";
-// import bcrypt from "bcrypt";
-
-// const SALT_ROUNDS = 6;
-
-// const userSchema = new mongoose.Schema(
-//   {
-//     name: { type: String, required: true },
-//     email: { type: String, unique: true, trim: true, lowercase: true, required: true },
-//     password: { type: String, required: true }
-//   },
-//   {
-//     timestamps: true,
-//     toJSON: {
-//       transform(doc, ret) {
-//         delete ret.password;
-//         return ret;
-//       }
-//     }
-//   }
-// );
-
-// userSchema.pre("save", async function (next) {
-//   if (!this.isModified("password")) return next();
-//   this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
-//   next();
-// });
-
-// const User = mongoose.model("User", userSchema);
-// export default User;
 
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
