67e2a9d4f472a52ed499c2b96a3aa835e8c32c51
Tanisha <tthompsonjones8771@gmail.com>
2025-08-31 22:12:30 -0400

    Update code in ensureLoggedIn.js

--- backend/middleware/ensureLoggedIn.js
+++ backend/middleware/ensureLoggedIn.js
@@ -1,18 +1,3 @@
-// module.exports = function (req, res, next) {
-//   if (req.user) return next();
-//   res.status(401).json({ message: 'Unauthorized' });
-// };
-
-// // backend/middleware/ensureLoggedIn.js
-// export default function ensureLoggedIn(req, res, next) {
-//   // check if user is logged in
-//   next();
-// }
-
-// export default function ensureLoggedIn(req, res, next) {
-//   if (!req.user) return res.status(401).json({ message: "Not logged in" });
-//   next();
-// }
 
 export function ensureLoggedIn(req, res, next) {
   if (req.user) {
