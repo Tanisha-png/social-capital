9f2e34dd11a16e0e1bda394c5293e26d1178b1ac
Tanisha <tthompsonjones8771@gmail.com>
2025-09-12 17:41:36 -0400

    Update code in db.js

--- backend/db.js
+++ backend/db.js
@@ -6,19 +6,4 @@ mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 });
 
-// import mongoose from "mongoose";
 
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error(`❌ MongoDB connection error: ${error.message}`);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
