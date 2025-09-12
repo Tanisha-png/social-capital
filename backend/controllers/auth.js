efabf39c573611822ee325db00e95ca1447e09e2
Tanisha <tthompsonjones8771@gmail.com>
2025-09-12 17:36:16 -0400

    Update code in auth.js

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -22,22 +22,6 @@ export const signUp = async (req, res) => {
   }
 };
 
-// export const logIn = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-//     const user = await User.findOne({ email });
-//     if (!user) return res.status(400).json({ message: "Invalid credentials" });
-
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
-
-//     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
-
-//     res.json({ token, user });
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
 
 export const logIn = async (req, res) => {
   try {
