93e059e02885cb3cbf2b3249ec5d7d58dcaa052c
Tanisha <tthompsonjones8771@gmail.com>
2025-09-22 11:45:02 -0400

    Update code in posts.js

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,32 +1,4 @@
 
-
-// // backend/routes/posts.js
-// import express from "express";
-// import {
-//     getPosts,
-//     getPostById,
-//     createPost,
-//     deletePost,
-//     addReply,
-//     toggleLike,
-// } from "../controllers/posts.js";
-// import checkToken from "../middleware/checkToken.js";
-
-// const router = express.Router();
-
-// // ✅ All post routes require valid token
-// router.use(checkToken);
-
-// router.get("/", getPosts);
-// router.get("/:id", getPostById);
-// router.post("/", createPost);
-// router.delete("/:id", deletePost);
-// router.post("/:id/replies", addReply);
-// router.post("/:id/like", toggleLike);
-
-// export default router;
-
-
 import express from "express";
 import { ensureLoggedIn } from "../middleware/ensureLoggedIn.js";
 import Notification from "../models/Notification.js";
