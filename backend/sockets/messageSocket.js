445baeb1be0547a1247a89bd2ad9544ced062f69
Tanisha <tthompsonjones8771@gmail.com>
2025-11-08 19:47:49 -0500

    Update code in sockets/messageSocket.js

--- backend/sockets/messageSocket.js
+++ backend/sockets/messageSocket.js
@@ -1,22 +1,4 @@
 
-// // backend/sockets/messageSocket.js
-// export default function setupMessageSocket(io) {
-//     io.on("connection", (socket) => {
-//         console.log("A user connected:", socket.id);
-
-//         // Example: receive a message from client
-//         socket.on("sendMessage", (data) => {
-//             console.log("Received message:", data);
-//             // Broadcast to all connected clients
-//             io.emit("receiveMessage", data);
-//         });
-
-//         socket.on("disconnect", () => {
-//             console.log("User disconnected:", socket.id);
-//         });
-//     });
-// }
-
 export default function setupMessageSocket(io) {
     io.on("connection", (socket) => {
         console.log("✅ User connected:", socket.id);
