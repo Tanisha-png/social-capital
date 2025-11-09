10ac2f63075887529479fcac2236c082420b700c
Tanisha <tthompsonjones8771@gmail.com>
2025-11-08 20:08:00 -0500

    Update code in services/notificationApi.js

--- frontend/src/services/notificationApi.js
+++ frontend/src/services/notificationApi.js
@@ -1,14 +1,3 @@
-// services/notificationApi.js
-// export const markMessageNotificationsRead = async (senderId, token) => {
-//     await fetch(`${import.meta.env.VITE_API_BASE}/messages/mark-read`, {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//             Authorization: `Bearer ${token}`,
-//         },
-//         body: JSON.stringify({ senderId }),
-//     });
-// };
 
 export const markMessageNotificationsRead = async (senderId, token) => {
     const res = await fetch(`/api/messages/mark-read`, {
