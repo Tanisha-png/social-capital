00e139ca05a6a541d0f6aaf6f87a6a4b1ce564bd
Tanisha <tthompsonjones8771@gmail.com>
2025-11-10 17:59:09 -0500

    Update code in auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 
 import express from "express";
-import User from "../models/User.js";
+import User from "../models/user.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import upload from "../middleware/upload.js";
