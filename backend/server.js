9240fb942cd2118f671e783b63d32fd9cb5b4277
Tanisha <tthompsonjones8771@gmail.com>
2025-11-11 15:18:11 -0500

    Update code in server.js

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,11 @@ dotenv.config();
 await connectDB();
 
 const app = express();
+app.set('trust proxy', 1);
 const server = http.createServer(app);
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://social-capital-1f13c371b2ba.herokuapp.com/"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
