01db2df511ae9a30bfec530e486ec3b374e236ce
Tanisha <tthompsonjones8771@gmail.com>
2025-11-11 17:24:18 -0500

    Update code in userApi.js

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api";
 const USERS_URL = `${BASE_URL}/users`;
 
 // ==============================
