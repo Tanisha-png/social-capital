477a7d26e600b3c5d44af6d58c4bf8329687471f
Tanisha <tthompsonjones8771@gmail.com>
2025-12-03 18:55:52 -0500

    Install bcryptjs

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,55 @@
 
+// import mongoose from "mongoose";
+// import bcrypt from "bcrypt";
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     name: { type: String, required: true },
+//     email: { type: String, required: true, unique: true },
+//     password: { type: String, required: true },
+
+//     // Profile fields
+//     firstName: { type: String, default: "" },
+//     lastName: { type: String, default: "" },
+//     bio: { type: String, default: "" },
+//     occupation: { type: String, default: "" },
+//     education: { type: String, default: "" },
+//     canHelpWith: [{ type: String }],
+//     needHelpWith: [{ type: String }],
+//     location: { type: String, default: "" },
+//     avatar: { type: String, default: "/default-avatar.png" }, // optional profile pic
+
+//     // Social connections
+//     friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+//     friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+//     createdAt: { type: Date, default: Date.now },
+//   },
+//   { timestamps: true }
+// );
+
+// // Hash password before saving
+// userSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//     next();
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+// // Compare entered password with hashed one
+// userSchema.methods.matchPassword = async function (enteredPassword) {
+//   return bcrypt.compare(enteredPassword, this.password);
+// };
+
+// // const User = mongoose.model("User", userSchema);
+// const User = mongoose.models.User || mongoose.model("User", userSchema);
+// export default User;
+
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,7 +66,7 @@ const userSchema = new mongoose.Schema(
     canHelpWith: [{ type: String }],
     needHelpWith: [{ type: String }],
     location: { type: String, default: "" },
-    avatar: { type: String, default: "/default-avatar.png" }, // optional profile pic
+    avatar: { type: String, default: "/default-avatar.png" },
 
     // Social connections
     friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
@@ -30,6 +79,7 @@ const userSchema = new mongoose.Schema(
 // Hash password before saving
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
+
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
@@ -41,9 +91,8 @@ userSchema.pre("save", async function (next) {
 
 // Compare entered password with hashed one
 userSchema.methods.matchPassword = async function (enteredPassword) {
-  return bcrypt.compare(enteredPassword, this.password);
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
-// const User = mongoose.model("User", userSchema);
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 export default User;
